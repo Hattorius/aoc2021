e5b1ad4d926a878ba9b4e72b994e772e880d3a11
Aaron Jonk <AJ@jonk.name>
2021-12-08 00:25:22 +0100

    Day 6 - 1

--- day 6/1.py
+++ day 6/1.py
@@ -1,4 +1,20 @@
 
 # get input into list
 with open('./input.txt') as f:
-    fishes = f.read().splitlines()
+    fishes = [int(line) for line in f.read().splitlines()[0].split(',')]
+
+days = 80
+for _ in range(days):
+    temp_fishes = []
+    new = 0
+    for fish in fishes:
+        if fish > 0:
+            temp_fishes.append(fish - 1)
+            continue
+        temp_fishes.append(6)
+        new += 1
+    for _ in range(new):
+        temp_fishes.append(8)
+    fishes = temp_fishes
+
+print(len(fishes))
